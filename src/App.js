e360ca392c483f2561d128ddf14df0a0748b96c4
suryamani7 <suryamanikumar344@gmail.com>
2023-03-18 19:36:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.scss"
+import Home from "./Component/Home";
+import Header from "./Component/Header/Header";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element = {<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Component/Row/Row.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from './Card'
+
+
+const imgUrl = "https://image.tmdb.org/t/p/original";
+
+const Row = ({
+  title, 
+  arr = [
+  ],
+}) => {
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div>
+
+        {
+          arr.map((item, index) => (
+            <Card key={index} img={`${imgUrl}/${item.poster_path}`}/>
+          ))
+        }
+      
+      </div>
+    </div>
+  )
+}
+
+export default Row
